ee1d7aa5815d17a23a408377c0f1e838180fcfd3
taeseok0712 <taeseok0712@naver.com>
2021-10-06 16:14:06 +0900

    drill7
    https://youtu.be/mBeF-6mow1w

--- Lecture06_HandlingInputs/drill7.py
+++ Lecture06_HandlingInputs/drill7.py
@@ -1,6 +1,4 @@
 from pico2d import *
-
-
 def handle_events():
     global running
     global dir
@@ -14,23 +12,20 @@ def handle_events():
             mouse_x, mouse_y = events.x, 600 - 1 - events.y
             if(mouse_x > 400):
                 dir = 1
+                ani = 1
 
             elif (mouse_x <= 400):
                 dir = 1
+                ani = 0
 
 
             elif events.key == SDLK_ESCAPE:
                 running= False;
-
-
     pass
-
-
 open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 mouse = load_image('hand_arrow.png')
-
 running = True
 frame = 0
 dir = 0 # -1 left,+1 right
@@ -44,9 +39,7 @@ while running:
     grass.draw(400, 30)
     character.clip_draw(frame * 100, 100 * ani, 100, 100, x, y)
     mouse.clip_draw(0, 0, 50, 52, mouse_x, mouse_y)
-
     update_canvas()
-
     handle_events()
     frame = (frame + 1) % 8
     if (x<mouse_x):
@@ -59,8 +52,6 @@ while running:
             y += dir * 5
     elif (y >= mouse_y):
             y -= dir * 5
-
     delay(0.01)
-
+    
 close_canvas()
-
